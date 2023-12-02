6e7f7d04c17f5b120e046504b8d47150321b37fe
BradHacker <harkerbd@gmail.com>
2023-12-02 18:22:56 -0500

    Working ecommerce example

--- go.mod
+++ go.mod
@@ -3,16 +3,16 @@ module github.com/TrustedPay/example-ecommerce
 go 1.21.1
 
 require (
-	github.com/TrustedPay/tp-term v0.0.0-20231202000309-e92d134f8241
+	github.com/TrustedPay/tp-term v0.0.0-20231202000653-d2ed6a84f9fa
 	github.com/sirupsen/logrus v1.9.3
 	google.golang.org/grpc v1.59.0
 )
 
 require (
 	github.com/golang/protobuf v1.5.3 // indirect
-	golang.org/x/net v0.14.0 // indirect
-	golang.org/x/sys v0.11.0 // indirect
-	golang.org/x/text v0.12.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20230822172742-b8732ec3820d // indirect
+	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231127180814-3a041ad873d4 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"io"
 	"net"
+	"net/http"
 
 	"github.com/TrustedPay/tp-term/pkg/tpterm"
 	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
+
+type NonceResponse struct {
+	Nonce         int64  `json:"nonce"`
+	TransactionID string `json:"transactionId"`
+}
+
+type AuthorizeRequest struct {
+	TransactionID string              `json:"transactionId"`
+	Request       *tpterm.Transaction `json:"req"`
+	Signature     []byte              `json:"signature"`
+}
 
 func main() {
 	// Generate the TP Term gRPC Client
@@ -27,11 +42,25 @@ func main() {
 	// Context for operations
 	ctx := context.Background()
 
-	// TODO: request nonce from bank
+	resp, err := http.Get("http://localhost:8080/transaction/initialize")
+	if err != nil {
+		logrus.Fatalf("failed to get nonce from bank: %v", err)
+	}
+	defer resp.Body.Close()
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		logrus.Fatalf("failed to read nonce body: %v", err)
+	}
+	var nonceResp NonceResponse
+	err = json.Unmarshal(bodyBytes, &nonceResp)
+	if err != nil {
+		logrus.Fatalf("failed to parse nonce response: %v", err)
+	}
+	logrus.Infof("Got nonce %d for transaction %s from bank", nonceResp.Nonce, nonceResp.TransactionID)
 
 	// Generate a transaction
 	transaction := tpterm.Transaction{
-		Nonce:           0,
+		Nonce:           nonceResp.Nonce,
 		TransactionData: []byte{},
 		Amount:          100,
 		CardNumber:      "8888888888888888",
@@ -39,6 +68,7 @@ func main() {
 	}
 	logrus.Printf("Attempting transaction of $%.2f on card %s (Exp. %s)", float64(transaction.Amount)/100, transaction.CardNumber, transaction.CardExp)
 
+	logrus.Infof("Sending request to TP Term for transaction signing...")
 	// Ask TP Term to sign transaction
 	tpSignature, err := tpClient.SignRequest(ctx, &transaction)
 	if err != nil {
@@ -46,5 +76,30 @@ func main() {
 		logrus.Fatalf("TP Term failed signature, cancelling transaction...")
 	}
 
-	logrus.Printf("Got valid signature for transaction: %x", tpSignature.TransactionSignature)
+	logrus.Printf("Got valid signature for transaction:\nDigest: %x\nSignature: %x", tpSignature.TransactionDigest, tpSignature.TransactionSignature)
+
+	logrus.Printf("Attempting to authorize transaction...")
+
+	request := AuthorizeRequest{
+		TransactionID: nonceResp.TransactionID,
+		Request:       &transaction,
+		Signature:     tpSignature.TransactionSignature,
+	}
+	requestBytes, err := json.Marshal(request)
+	if err != nil {
+		logrus.Fatalf("failed to marshal authorize request bytes")
+	}
+
+	resp, err = http.Post("http://localhost:8080/transaction/authorize", "application/json", bytes.NewBuffer(requestBytes))
+	if err != nil {
+		logrus.Fatalf("failed to POST authorize request: %v", err)
+	}
+
+	if resp.StatusCode == 200 {
+		logrus.Infof("Transaction successful!")
+	} else if resp.StatusCode == 403 {
+		logrus.Warnf("Transaction failed. Not enough funds...")
+	} else {
+		logrus.Errorf("Something went wrong. Please check the logs...")
+	}
 }
